3f9323ad18e4adbe70299c9a791da56b2571da9e
55973 <55973@etu.he2b.be>
2022-12-20 14:36:26 +0100

    1 et 2

---
+++ public/js/main.js
@@ -0,0 +1,76 @@
+
+
+let jour = parseInt(prompt('entrez un jour'))
+switch (jour){
+    case 1:
+        alert('Lundi');
+        break;
+    case 2:
+        alert('Mardi');
+        break;
+        
+    case 3:
+        alert('Mercredi')
+        break;
+
+    case 4:
+        alert('jeudi');
+        break;
+            
+    case 5:
+         alert('vendd');
+         break;
+    
+    case 6:
+        alert('smdi');
+        break;
+
+    case 7:
+        alert ('dim');
+        break;
+
+    default:
+        alert('entre 1 et 7')
+
+}
+
+
+let mois = parseInt(prompt('mois?'))
+switch (mois){
+    case 1:
+        alert('janvier');
+    break;
+    case 2:
+        alert('fev');
+    break;
+    case 3:
+        alert('mars');
+    break;
+    case 4:
+        alert('avril');
+    break;
+    case 5:
+        alert('mai');
+    break;
+    case 6:
+        alert('juin');
+    break;
+    case 7:
+        alert('juill');
+    break;
+    case 8:
+        alert('aout');
+    break;
+    case 9:
+        alert('sept');
+    break;
+    case 10:
+        alert('oct');
+    break;
+    case 11:
+        alert('nov');
+    break;
+    case 12:
+        alert('Dec');
+
+}
