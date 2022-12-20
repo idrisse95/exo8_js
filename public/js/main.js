e9fe328e31b3175e7422a44c04a0af673f02f0b8
55973 <55973@etu.he2b.be>
2022-12-20 14:41:45 +0100

    exo 3

--- public/js/main.js
+++ public/js/main.js
@@ -1,76 +1,102 @@
 
 
-let jour = parseInt(prompt('entrez un jour'))
-switch (jour){
-    case 1:
-        alert('Lundi');
-        break;
-    case 2:
-        alert('Mardi');
-        break;
+// let jour = parseInt(prompt('entrez un jour'))
+// switch (jour){
+//     case 1:
+//         alert('Lundi');
+//         break;
+//     case 2:
+//         alert('Mardi');
+//         break;
         
-    case 3:
-        alert('Mercredi')
-        break;
+//     case 3:
+//         alert('Mercredi')
+//         break;
 
-    case 4:
-        alert('jeudi');
-        break;
+//     case 4:
+//         alert('jeudi');
+//         break;
             
-    case 5:
-         alert('vendd');
-         break;
+//     case 5:
+//          alert('vendd');
+//          break;
     
-    case 6:
-        alert('smdi');
-        break;
+//     case 6:
+//         alert('smdi');
+//         break;
 
-    case 7:
-        alert ('dim');
-        break;
+//     case 7:
+//         alert ('dim');
+//         break;
 
-    default:
-        alert('entre 1 et 7')
+//     default:
+//         alert('entre 1 et 7')
 
-}
+// }
 
 
-let mois = parseInt(prompt('mois?'))
-switch (mois){
-    case 1:
-        alert('janvier');
-    break;
-    case 2:
-        alert('fev');
-    break;
-    case 3:
-        alert('mars');
-    break;
-    case 4:
-        alert('avril');
-    break;
-    case 5:
-        alert('mai');
-    break;
-    case 6:
-        alert('juin');
-    break;
-    case 7:
-        alert('juill');
-    break;
-    case 8:
-        alert('aout');
-    break;
-    case 9:
-        alert('sept');
-    break;
-    case 10:
-        alert('oct');
-    break;
-    case 11:
-        alert('nov');
-    break;
-    case 12:
-        alert('Dec');
+// let mois = parseInt(prompt('mois?'))
+// switch (mois){
+//     case 1:
+//         alert('janvier');
+//     break;
+//     case 2:
+//         alert('fev');
+//     break;
+//     case 3:
+//         alert('mars');
+//     break;
+//     case 4:
+//         alert('avril');
+//     break;
+//     case 5:
+//         alert('mai');
+//     break;
+//     case 6:
+//         alert('juin');
+//     break;
+//     case 7:
+//         alert('juill');
+//     break;
+//     case 8:
+//         alert('aout');
+//     break;
+//     case 9:
+//         alert('sept');
+//     break;
+//     case 10:
+//         alert('oct');
+//     break;
+//     case 11:
+//         alert('nov');
+//     break;
+//     case 12:
+//         alert('Dec');
 
-}
+// }
+
+
+// let note = parseInt(prompt('note?'))
+// switch (note){
+//     case 100:
+//         alert('A');
+//     break;
+//     case 90:
+//         alert('B');
+//     break;
+//     case 80:
+//         alert('C');
+//     break;
+//     case 70:
+//         alert('D');
+//     break;
+//     case 60:
+//         alert('E');
+//     break;
+//     case 50:
+//         alert('F');
+//     default:
+//         alert('c mort')
+// }
+
+
